86edcb7d3c31dce75e537e8873b53edd3b78af59
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-07 19:42:49 -0400

    get /api/users

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+const express = require('express');
+
+const Data = require('./data/db.js');
+
+const port = 5000;
+
+const server = express();
+
+server.get('/api/users', (req, res) => {
+  Data.find()
+    .then(users => {
+      users
+        ? res.json(users)
+        : res.status(418).json({ message: 'invalid as heck' });
+    })
+    .catch(err => {
+      res.status(500).json({
+        err: err,
+        message: 'failed to retrieve users'
+      });
+    });
+});
+
+server.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
